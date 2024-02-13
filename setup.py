d2e1b4b663dbff2458a0f4b7ad05f142e55f9793
suraj pawar <surajpawar558@gmail.com>
2024-02-14 04:16:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_e_dot="-e ."
+def get_requirements(file_path: str) -> List[str]:
+    """This function will return a list of requirements."""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    return requirements
+
+setup(
+    REPO_NAME="telecomproject",
+    version="0.0.1",
+    author="suraj",
+    author_email="surajpawar558@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
